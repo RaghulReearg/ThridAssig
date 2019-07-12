bddf9571d266ebe7bb5998ffba16822994f052ff
RaghulReearg <38530994+RaghulReearg@users.noreply.github.com>
2019-07-12 18:16:33 +0530

    commitit

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var cont = document.getElementById("cont");
+var ques = document.getElementById("ques");
+
 var output = "";
-for (var i = 0; i < 4; i++) {
-  cont.innerHTML = output +=
-    '<input type="radio" value="1" name="choice">1</input><input type="radio" value="2" name="choice">2</input><input type="radio" value="3" name="choice">3</input><br/>';
+var question = "";
+var questions = ["question 1", "Question 2", "question 3"];
+var options = [
+  ["option1", "option2", "option3", "option4"],
+  ["optiona", "optionb", "optionc", "optiond"],
+  ["optionz", "optionx", "optiony", "optionw"]
+];
+for (var i = 0; i < 3; i++) {
+  var j = 0;
+  cont.innerHTML = output += `${
+    questions[i]
+  }<br><input type="radio" value="1" name="choice">${
+    options[i][j]
+  }</input><input type="radio" value="2" name="choice">${
+    options[i][j + 1]
+  }</input><input type="radio" value="3" name="choice">${
+    options[i][j + 2]
+  }</input><br/>`;
 }
 var radios = document.getElementsByName("choice");
 console.log(radios);
